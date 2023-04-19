f52cdb32f000e45bf7b79e480e4f252f616c2433
kazenoshin <hiratakazenoshin@gmail.com>
2023-04-20 08:29:33 +0900

    2023-04/20-08:29 last page is 226

--- test/gate_test.rb
+++ test/gate_test.rb
@@ -4,20 +4,20 @@ require_relative '../lib/ticket'
 
 
 class GateTest < Minitest::Test
-    # def test_gate
-    #     umeda = Gate.new(:umeda)
-    #     juso = Gate.new(:juso)
-    #     ticket = Ticket.new(150)
-    #     umeda.enter(ticket)
-    #     assert juso.exit(ticket)
-    # end
-
-    def test_umeda?_to_mikuni_when_fare_is_not_enough_
+    def test_gate
         umeda = Gate.new(:umeda)
-        mikuni = Gate.new(:mikuni)
-
+        juso = Gate.new(:juso)
         ticket = Ticket.new(150)
         umeda.enter(ticket)
-        refute mikuni.exit(ticket)
+        assert juso.exit(ticket)
     end
+
+    # def test_umeda_to_mikuni_when_fare_is_not_enough_
+    #     umeda = Gate.new(:umeda)
+    #     mikuni = Gate.new(:mikuni)
+
+    #     ticket = Ticket.new(150)
+    #     umeda.enter(ticket)
+    #     refute mikuni.exit(ticket)
+    # end
 end
